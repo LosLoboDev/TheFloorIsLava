368691a8e07a6ceb4d122cf58d25eee50407d1d4
tdail <dail.3@osu.edu>
2017-04-21 14:21:45 -0400

    Updated portal prefab
    Can now assign a teleport location to each instance

---
+++ TheFloorIsLava/Assets/Scripts/teleport.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class teleport : MonoBehaviour {
+
+	public Vector3 newPosition = new Vector3 (0, 0, 0);
+
+	void OnCollisionStay(Collision collisionInfo) {
+		if (collisionInfo.gameObject.name == "FPSController") {
+			collisionInfo.gameObject.transform.position = newPosition;
+		}
+	}
+}
