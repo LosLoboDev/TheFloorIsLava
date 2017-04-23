65dd341a8835af5b528ac45fba4b56fbde9a1280
Alex Barnes <barnes.778@osu.edu>
2017-04-23 11:56:35 -0400

    Added (very basic) wind mechanic in wind_scene
    Still need to find a way to make it an event and trigger it in certain
    places of the scene, instead of just all the time.

---
+++ TheFloorIsLava/Assets/Scripts/wind.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wind : MonoBehaviour {
+
+    Rigidbody rb;
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //translate in certain direction
+        rb.transform.Translate(Vector3.left * Time.deltaTime, Space.World);
+    }
+}
