84bd9419c8a8cd61de425454be0c7870bce27fe8
Alex Barnes <barnes.778@osu.edu>
2017-05-01 17:47:28 -0400

    Added reset and "death"
    Press r to start restart at starting position.
    
    If the lava is collided with, the player is teleported to start.

---
+++ TheFloorIsLava/Assets/Scripts/Reset.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reset : MonoBehaviour {
+
+    public Vector3 pos = new Vector3(-1.73f, 7.41f, -13.54f);
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.R))
+        {
+            gameObject.transform.position = pos;
+        }
+	}
+}
--- TheFloorIsLava/Assets/Scripts/death_on_impact.cs
+++ TheFloorIsLava/Assets/Scripts/death_on_impact.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class death_on_impact : MonoBehaviour
 {
-    public Vector3 newPosition = new Vector3(0, 0, 0);
+    public Vector3 newPosition = new Vector3(-1.73f, 7.41f, -13.54f);
 
-    void OnCollisionStay(Collision collisionInfo)
+    void OnTriggerEnter(Collider collisionInfo)
     {
-        Debug.Log(collisionInfo.gameObject.name);
         if (collisionInfo.gameObject.name == "FPSController")
         {
             collisionInfo.gameObject.transform.position = newPosition;
-            Debug.Log("You died");
+            Debug.Log("You died, restart!");
         }
     }
 }
