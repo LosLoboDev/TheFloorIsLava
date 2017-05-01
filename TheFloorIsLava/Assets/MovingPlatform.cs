fe9fdbaaa5901225ac1c049be2696ea074ccf5af
Alex Barnes <barnes.778@osu.edu>
2017-05-01 19:41:58 -0400

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	TheFloorIsLava/Library/CurrentLayout.dwlt
    #	TheFloorIsLava/Library/InspectorExpandedItems.asset
    #	TheFloorIsLava/Library/ScriptAssemblies/Assembly-CSharp.dll
    #	TheFloorIsLava/Library/ScriptAssemblies/Assembly-CSharp.dll.mdb
    #	TheFloorIsLava/Library/assetDatabase3
    
    #	TheFloorIsLava/Library/metadata/15/15135e86e52ab4b038dcbd61d7549e9f.info
    
    #	TheFloorIsLava/Library/metadata/2f/2fe3476eabbbb6c448e6b55a2cc471f5.info
    
    #	TheFloorIsLava/Library/metadata/31/3188ffbd641642a4ca56b1de8bbbca6f.info
    
    #	TheFloorIsLava/Library/metadata/97/9721ab373e27146389ae58af7f9ce3cb.info
    
    #	TheFloorIsLava/Library/metadata/d5/d5bbfe17fd61a7f45ad1932a5dd14b56.info
    
    #	TheFloorIsLava/Library/metadata/d7/d7ea5a218770df14895b81e3602e420f.info
    #	TheFloorIsLava/Library/shadercompiler-UnityShaderCompiler.exe0.log
    
    Added rotating platforms and more to lava_scene

--- TheFloorIsLava/Assets/MovingPlatform.cs
+++ TheFloorIsLava/Assets/MovingPlatform.cs
@@ -6,30 +6,38 @@ public class MovingPlatform : MonoBehaviour {
 
 	public bool isMoving = false;
 	public float speed = 0.05f;
-	int updateCount = 0;
-	int xDistance = 80;
+	Vector3 startPos;
+	public Vector3 endPos = new Vector3(0,0,0);
+	Vector3 direction;
+	float distance;
 
 	// Use this for initialization
 	void Start () {
-		
+		startPos  = gameObject.transform.position;
+		direction = (endPos - startPos).normalized;
+		distance = (endPos - startPos).magnitude;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (isMoving) {
-			gameObject.transform.Translate (new Vector3 (speed, 0, 0));
-			updateCount++;
-			if (updateCount == xDistance) {
-				speed = -speed;
-				updateCount = 0;
+			gameObject.transform.Translate (direction * speed);
+			if (currentDistance() >= distance) {
+				direction = -direction;
+				Vector3 temp = startPos;
+				startPos = gameObject.transform.position;
+				endPos = temp;
+				distance = (endPos - startPos).magnitude;
 			}
 		}
 	}
 
-	void OnCollisionStay(Collision collisionInfo) {
-		if (collisionInfo.gameObject.name == "FPSController") {
-			Debug.Log ("on platform!");
-			collisionInfo.gameObject.transform.Translate (new Vector3 (speed, 0, 0));
-		}
+	float currentDistance() {
+		return (gameObject.transform.position - startPos).magnitude;
 	}
+//	void OnCollisionStay(Collision collisionInfo) {
+//		if (collisionInfo.gameObject.name == "FPSController") {
+//			collisionInfo.gameObject.transform.Translate (new Vector3 (speed, 0, 0));
+//		}
+//	}
 }
---
+++ TheFloorIsLava/Assets/Scripts/RotateObject.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateObject : MonoBehaviour {
+
+    bool yes = true;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+            RotateObjectSlow();
+    }
+
+    void RotateObjectSlow()
+    {
+        transform.Rotate(Time.deltaTime * 30, 0, 0);
+    }
+}
